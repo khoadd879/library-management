dca7dd41fcd6bf3afdea792a2d1fa5fd7f91091a
TienKhang <nguyentienkhang09022005@gmail.com>
2025-06-30 19:21:13 +0700

    final update

--- se104-be/LibraryManagement/LibraryManagement/Helpers/AppRoles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LibraryManagement.Helpers
-{
-    public static class AppRoles
-    {
-        public const string Admin = "Administrator";
-        public const string Reader = "Reader";
-    }
-}
